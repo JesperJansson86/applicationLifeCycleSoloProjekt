735e0b62cfd5813519029c5916f1019ffd26528b
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2021-05-11 11:18:57 +0200

    Added DrinkController

---
+++ spring-docker-demo/src/main/java/com/example/springdocker/controller/DrinkController.java
@@ -0,0 +1,32 @@
+package com.example.springdocker.controller;
+
+import com.example.springdocker.model.Drink;
+import com.example.springdocker.service.DrinkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class DrinkController {
+    private final DrinkService service;
+
+    @GetMapping("/Drinks")
+    public List<Drink> getDrinks() {
+        return service.getDrinks();
+    }
+
+    @PostMapping("/Drinks")
+    public void saveNewDrink(@RequestBody Drink Drink) {
+        service.saveNewDrink(Drink);
+    }
+
+    @GetMapping("/Drinks/cookable")
+    public List<String> getCookableoods() {
+        return service.getCookableDrinks();
+    }
+}
