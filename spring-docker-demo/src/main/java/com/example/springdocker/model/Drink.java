e91e18bac60f0f7c8a2cf6a616d8869522415f65
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2021-05-11 11:09:07 +0200

    Added drink.java

---
+++ spring-docker-demo/src/main/java/com/example/springdocker/model/Drink.java
@@ -0,0 +1,16 @@
+package com.example.springdocker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Drink {
+        String id;
+        String name;
+        boolean delicious;
+        boolean canIDrinkIt;
+
+}
